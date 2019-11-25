776149cf5d1f0274f2dceedd92b19f36383f4ad9
penrodlol <penrod.christian7984@gmail.com>
2019-11-24 21:12:42 -0500

    Fixing syntax errors in delta cookie table creations sqls

--- deltas/create/create-cookie-map-container.sql
+++ deltas/create/create-cookie-map-container.sql
@@ -1,6 +1,6 @@
-CREATE TABLE fullstackhandyman.cookie_maps_container
+CREATE TABLE `fullstackhandyman`.`cookie_maps_container`
 (
-    container_num INT NOT NULL AUTO_INCREAMENT UNIQUE,
-    name [VARCHAR](150) NOT NULL UNIQUE,
-    PRIMARY_KEY (container_num)
+    container_num INT NOT NULL auto_increment UNIQUE,
+    name VARCHAR(150) NOT NULL UNIQUE,
+    primary key (container_num)
 );
--- deltas/create/create-cookie-maps.sql
+++ deltas/create/create-cookie-maps.sql
@@ -1,8 +1,8 @@
 CREATE TABLE fullstackhandyman.cookie_maps
 (
-    map_num INT NOT NULL AUTO_INCREAMENT UNIQUE,
+    map_num INT NOT NULL auto_increment UNIQUE,
     container_num INT NOT NULL,
-    name [VARCHAR](150) NOT NULL UNIQUE,
-    PRIMARY_KEY (map_num),
-    FOREIGN_KEY (container_num) REFERENCES cookie_maps_container(container_num)
+    name VARCHAR(150) NOT NULL UNIQUE,
+    primary key (map_num),
+    foreign key (container_num) REFERENCES cookie_maps_container(container_num)
 );
