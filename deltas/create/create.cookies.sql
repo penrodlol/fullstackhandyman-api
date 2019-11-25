662fcb8d1b5cd5415171216b17e8095cf35fe596
penrodlol <penrod.christian7984@gmail.com>
2019-11-24 20:00:19 -0500

    Initial commit for deployments branch

--- deltas/create/create.cookies.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE fullstackhandyman.cookies
-(
-    cookie_num INT NOT NULL AUTO_INCREAMENT UNIQUE,
-    map_num INT NOT NULL,
-    name [VARCHAR](150) NOT NULL,
-    value [VARCHAR](150) NOT NULL,
-    PRIMARY_KEY (cookie_num),
-    FOREIGN_KEY (map_num) REFERENCES cookie_maps(map_num)
-);
